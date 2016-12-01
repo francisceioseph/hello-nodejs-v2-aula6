406a36a6675a6d33aa52d41ac94a3323f1706701
Francis Souza <francisceioseph@gmail.com>
2016-12-01 15:24:45 -0300

    Requisições javascript - Requisições com Javascript usando JQuery. - Requisições com Javascript usando Axios

--- appweb/index.js
+++ appweb/index.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 app.get('/hello', (req, res) => {
     res.send('hello');
@@ -19,6 +20,16 @@ app.post('/adivinha', (req, res) => {
     console.log("os dados recebidos do formulário são:");
     console.log(req.body);
     res.send("<h1>Sucesso!</h1><a href='numero.html'>voltar</a>");
+});
+
+app.get('/convidados', (req, res) => {
+    let convidados = [
+        {nome: 'Socorro Feirante' },
+        {nome: 'Miriam da Mary Key'},
+        {nome: 'Silvan da Silva Feitosa'},
+    ];
+
+    res.send(convidados);
 });
 
 app.listen(3000);
