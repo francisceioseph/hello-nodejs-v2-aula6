197709569aca13747f65ed2abfc4d8e5702d23b2
Francis Souza <francisceioseph@gmail.com>
2016-12-01 15:02:58 -0300

    POST e Body Parser - Instalação e utilização do body-parser - Mudança de verbos http de get para post no template - Mudança de rota para post

--- appweb/index.js
+++ appweb/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/hello', (req, res) => {
     res.send('hello');
 });
 
-app.get('/dosave', (req, res) => {
+app.post('/dosave', (req, res) => {
     console.log("os dados recebidos do formulário são:");
-    console.log(req.query);
+    console.log(req.body);
     res.send("<h1>Sucesso!</h1><a href='index.html'>voltar</a>");
 });
 
