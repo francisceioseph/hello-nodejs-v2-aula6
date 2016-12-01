64a5fa0b6d0dee78706a1d1fe227235f8e2b15ed
Francis Souza <francisceioseph@gmail.com>
2016-12-01 14:57:19 -0300

    Express Static Files - Envio de HTML com form simples - Enviando informações do form utilizando metodo get - Checagem de url params

---
+++ appweb/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/hello', (req, res) => {
+    res.send('hello');
+});
+
+app.get('/dosave', (req, res) => {
+    console.log("os dados recebidos do formulário são:");
+    console.log(req.query);
+    res.send("<h1>Sucesso!</h1><a href='index.html'>voltar</a>");
+});
+
+app.listen(3000);
+
+console.log("app online");
